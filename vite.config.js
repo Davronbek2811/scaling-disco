95a4e0bd62a100605c5c54f397d0a0ce549beed9
Dvronjon <davronjon176@gamil.com>
2025-10-20 22:20:05 +0500

    deploy vite + tailwind via docs

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/scaling-disco/'
+  plugins: [react(), tailwindcss()],
+  base: '/scaling-disco/', // ⚠️ bu senga mos, repo noming bilan bir xil
+  build: {
+    outDir: 'docs', // ⚙️ GitHub Pages uchun
+  },
 })
